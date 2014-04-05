c32342835d93c2547027316380fec06b4ffc80ae
brisk <briskgreen@163.com>
2014-04-05 22:32:00 +0800

    更新

--- src/conf.c
+++ src/conf.c
@@ -1,9 +1,22 @@
 #include "conf.h"
 
-//解析对数
-//正确时返回0并置value为参数列表conf->len为参数个数
+//解析对参数
+//data为配置文件内容
+//正确时返回0并置res为参数列表conf->len为参数个数
 //错误时返回错误代码
-int parse_value(CONF *conf,CONF_VALUE **value);
+int parse_value(CONF *conf,char *data,CONF_VALUE **res);
+//得到下一行的位置
+int next_line(char *data);
+//得到参数的长度
+int get_value_len_with_normal(char *data);
+//得到键长度
+int get_key_len(char *data);
+//设置多个参数
+char *set_value_with_mul(char *data,int *len);
+//单引号特殊字符
+char *set_value_with_signal(char *data,int flags,int *len);
+//双绰号特殊字符
+char *set_value_with_double(char *data,int flags,int *len);
 
 //打开配置文件并初始化值
 //path为配置文件路径
@@ -38,7 +51,7 @@ CONF *conf_open(const char *path)
 //解析成功返回错误代码
 int conf_parse(CONF *conf)
 {
-	CONF_VALUE **value=NULL; //参数对列表
+	CONF_VALUE *value=NULL; //参数对列表
 	char *data; //配置文件内容
 	long file_len; //配置文件长度
 	int retcode;
@@ -59,7 +72,7 @@ int conf_parse(CONF *conf)
 	fclose(fp);
 
 	//解析 
-	retcode=parse_value(conf,value);
+	retcode=parse_value(conf,data,&value);
 
 	//开辟内存空间
 	conf->hash_data=malloc(sizeof(CONF_ARG)*conf->len);
@@ -244,4 +257,124 @@ void conf_error(int errcode)
 		default:
 			printf("未知错误!\n");
 	}
+}
+
+//解析参数
+int parse_value(CONF *conf,char *data,CONF_VALUE **res)
+{
+	int i;
+	int len;
+	int index=0; //存储键值对下标
+	int count=0; //多参数下标
+	int flags[4]={0};
+	CONF_VALUE *value=NULL;
+	/* 0 =
+	 * 1 '
+	 * 2 "
+	 * 3 ,
+	 */
+
+	for(i=0;data[i];++i)
+	{
+		len=0;
+
+		switch(data[i])
+		{
+			case '#':
+				i+=next_line(data+i);
+				break;
+			case '=':
+				if(flags[0])
+					flags[0]=1;
+				else
+					flags[0]=0;
+				break;
+			case ''':
+				if(!flags[2])
+					flags[1]=1;
+				else
+					flags[1]=0;
+				break;
+			case '"':
+				if(!flags[1])
+					flags[2]=1;
+				else
+					flags[2]=0;
+				break;
+			case ',':
+				if(flags[0])
+					flags[3]=1;
+				else
+					flags[3]=0;
+				break;
+		}
+
+		if(!flags[0] && data[i] != ' ' && data[i] != '=' && data[i] != '\t' && data[i] != '\n')
+		{
+			len=get_key_len(data+i);
+			if(len == -1)
+				return CONF_KEY_ERR;
+			value=realloc(value,sizeof(CONF_VALUE)*(index+1));
+			value[index].key=malloc(sizeof(char)*len+1);
+			snprintf(value[index].key,sizeof(char)*len+1,"%s",data+i);
+			i+=len;
+			flags[0]=1;
+		}
+
+		if(flags[0])
+		{
+			//过滤掉空白符以及符号
+			while(data[i] == ' ' || data[i] == '\t')
+				++i;
+			if((data[i] == '=') && (flags[1] || flags[2]))
+				++i;
+
+			if(!flags[1] && !flags[2] && !flags[3])
+			{
+				len=get_value_len_with_normal(data+i);
+				if(len == -1)
+					return CONF_VALUE_ERR;
+
+				value[index].value=realloc(sizeof(char)*len+1);
+				snprintf(value[index].value,sizeof(char)*len+1,"%s",data+i);
+				i+=len;
+			}
+			else if(flags[3] && (!flags[1] || !flags[2]))
+				value[index].value=set_value_with_mul(data+i,&i);
+			else if(flags[1])
+				value.[index].value=set_value_with_signal(data+i,flags[3],&i);
+			else if(flags[2])
+				value.[index].value=set_value_with_double(data+i,flags[3],&i);
+		}
+
+		++conf->len;
+	}
+
+	*res=value;
+}
+
+int next_line(char *data)
+{
+	int i=0;
+
+	while(data[i] != '\n')
+		++i;
+
+	return i+1;
+}
+
+int get_value_len(char *data)
+{}
+
+int get_key_len(char *data)
+{
+	int i=0;
+
+	while(data[i] != ' ' && data[i] != '=' && data[i] != '\t' && data[i]!= '\n')
+		++i;
+
+	if(data[i] == '\n')
+		return -1;
+
+	return i;
 }
