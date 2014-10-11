24f3a5d2441be6c0de8bb1bce616a33cdbd1ab7c
brisk <briskgreen@163.com>
2014-10-12 01:45:30 +0800

    更新散列表操作

--- src/hash/hash.h
+++ src/hash/hash.h
@@ -2,12 +2,23 @@
 #ifndef _HASH_H
 #define _HASH_H
 
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>*/
 #include "../conf.h"
 
 //插入数据,arg为hash数组，value为值,len为数组长度
-void conf_value_insert(CONF_ARG *arg,CONF_VALUE *value,int len);
+int conf_value_insert(CONF_ARG *arg,CONF_VALUE *value,int len);
+
+/* 复制哈希表
+ * new为新的哈表 
+ * new_len为新的哈表的大小
+ * old为旧的hash表
+ * old_len为旧hash表的大小
+ */
+void conf_hash_copy(CONF_ARG *new,int new_len,CONF_ARG *old,int old_len);
+
+//动态更新哈希表
+int conf_hash_update(CONF *conf);
+
+//置空哈希表
+void conf_hash_zero(CONF_ARG *arg,int len);
 
 #endif
