f83f687af3620c46f7d7a9715fc09b1ed863dfb9
brisk <briskgreen@163.com>
2014-10-12 01:59:59 +0800

    CONF添加size成员

--- src/conf.h
+++ src/conf.h
@@ -1,4 +1,6 @@
-/* conf-c一个基于c语言的轻量级读取/创建配置文件的库函数。
+/* VERSION : 1.1
+ * Date :2014-10-12
+ * conf-c一个基于c语言的轻量级读取/创建配置文件的库函数。
  * 该库使用MIT许可协议，被授权人有权利有使用、复制、修改、合并、出版发布、散布、再授权和/或贩售软件及软件的副本，及授予被供应人同等权利，惟服从以下义务。
  * 在软件和软件的所有副本中都必须包含以上版权声明和本许可声明。
  */
@@ -20,6 +22,9 @@
 #define CONF_NO_MEM -3 //申请内存出错
 #define CONF_KEY_ERR -4 //键错误
 #define CONF_VALUE_ERR -5 //值错误
+
+#define HASH_DEFAULT 100 //默认哈希表大小
+#define HASH_SP 0.7 //当前哈希表中数据个数大于等于70%时表大小动态增长70%
 
 /*值数据结构*/
 typedef struct conf_value
@@ -43,12 +48,14 @@ typedef struct arg
 /* conf-c的数据结构
  * fp为指向配置文件的FILE指针
  * len为键/值对个数
+ * size为哈希表大小
  * hash_data为存储键/值的数据结构
  */
 typedef struct
 {
 	FILE *fp;
-	int len;
+	size_t len;
+	size_t size;
 	CONF_ARG *hash_data;
 }CONF;
 
